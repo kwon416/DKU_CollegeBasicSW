9e87c0ea0e7496c093f35df50ea64970355f3d00
bubbletea03 <99546167+bubbletea03@users.noreply.github.com>
2022-11-24 17:32:15 +0900

    다중화면 시도중

--- ui/gui_manager.py
+++ ui/gui_manager.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("homeUI.ui")[0]
+form_class_home = uic.loadUiType("home.ui")[0]
+form_class_dkuMenu = uic.loadUiType("dku_menu.ui")[0]
 
-
-class WindowClass(QMainWindow, form_class):
+class WindowClass(QMainWindow, form_class_home):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -13,10 +13,19 @@ class WindowClass(QMainWindow, form_class):
         self.etaBtn.clicked.connect(self.onclick_eta)
 
     def onclick_dku(self):
-        print("단대 클릭함")
+        self.hide()
+        self.second = DkuMenu()
+        self.second.exec()
+        self.show()
 
     def onclick_eta(self):
         print("에타 클릭함")
+
+class DkuMenu(QDialog, QWidget, form_class_dkuMenu):
+    def __init__(self):
+        super(DkuMenu, self).__init__()
+        self.setupUi(self)
+        self.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
